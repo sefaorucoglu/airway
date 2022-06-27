1127bd86abd9e3663d832fbfa6b42083af911089
Onur Sefa ORUÇOĞLU <onursefa.orucoglu@sahabt.com>
2022-06-27 15:55:09 +0300

    FlightController created, GetInformationFlightRequest's name chanced with GetFlightInformationRequest FlightNotFoundException class created

--- src/main/java/controller/FlightController.java
+++ src/main/java/controller/FlightController.java
@@ -1,4 +1,60 @@
 package controller;
 
+import dto.request.flight.AddFlightRequest;
+import dto.request.flight.GetFlightInformationRequest;
+import dto.request.flight.UpdateFlightRequest;
+import dto.response.flight.FlightResponse;
+import exception.FlightNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.RequestScope;
+import service.FlightService;
+
+import java.util.List;
+import java.util.Optional;
+@RequestScope
+@RestController
+@RequestMapping("/Flights")
+@Validated
 public class FlightController {
+    private final service.FlightService flightService;
+
+    public FlightController(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
+
+    @PostMapping(value ="/add", produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Optional<FlightResponse> createFlight (@RequestBody @Valid AddFlightRequest request) {
+        return flightService.createFlight(request);
+    }
+    @DeleteMapping(value = "/delete/{FlightId}")
+    public Optional<FlightResponse> deleteFlightById (@PathVariable String FlightId){
+        return flightService.removeFlightById(FlightId);
+    }
+    @PutMapping(value="/update")
+    public Optional<FlightResponse> updateFlight (@RequestBody @Valid UpdateFlightRequest updateFlightRequest){
+        return flightService.updateFlight(updateFlightRequest);
+    }
+    @PostMapping("/getFlights")
+    public List<FlightResponse> findFlight (@RequestBody GetFlightInformationRequest request) {
+        if (request.getFlightDate().equals("")&&request.getFlightNumber().equals("")&&request.getFlightDate().equals("")&&
+        request.getFrom().equals("")&&request.getTo().equals("")&&request.getStatus().equals("")
+        ){
+            throw new FlightNotFoundException("Inputs could not be empty.");
+        }
+        return flightService.findFlights(request);
+    }
+    @GetMapping
+    public List<FlightResponse> findAllFlight (@RequestParam(defaultValue = "0",required = false) int pageNo,@RequestParam(defaultValue = "20",required = false) int pageSize){
+        return flightService.findAll(pageNo,pageSize);
+    }
+    @ExceptionHandler(value = FlightNotFoundException.class)
+    public ResponseEntity<Object> exception (FlightNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
 }
--- src/main/java/dto/request/booking/GetInformationBookingRequest.java
+++ src/main/java/dto/request/booking/GetInformationBookingRequest.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class GetInformationBookingRequest {
     private String BookingId;
